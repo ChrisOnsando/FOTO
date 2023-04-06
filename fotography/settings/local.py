7f298b1f74588204e4c325d98dc78e989e724639
ChrisOnsando <chrisnemwel@gmail.com>
2023-04-06 14:18:39 +0300

    chore(project): initialize project setup(#4)     * Set up project setup     * Set up project folder structure     * Start project fotogrpaphy     * Start project app

---
+++ fotography/settings/local.py
@@ -0,0 +1,8 @@
+from fotography.settings.base import ALLOWED_HOSTS
+
+DEBUG = True
+
+ALLOWED_HOSTS += [
+    "localhost",
+    "127.0.0.1",
+]
---
+++ fotography/settings/production.py
@@ -0,0 +1,3 @@
+from fotography.settings.base import ALLOWED_HOSTS  # noqa F401
+
+DEBUG = False
