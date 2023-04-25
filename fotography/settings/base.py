04aa2107d16005d67479815baad31cc1f7079b29
ChrisOnsando <chrisnemwel@gmail.com>
2023-04-25 17:33:19 +0300

    feat(project): add cloudinary(#9) 	*add cloudinary package 	*add cloudinary configuration

--- fotography/settings/base.py
+++ fotography/settings/base.py
@@ -171,7 +171,7 @@ SIMPLE_JWT = {
 
 # sendgrid settings
 EMAIL_HOST = "smtp.sendgrid.net"
-EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_USER = "FOTO"
 EMAIL_HOST_PASSWORD = config("SENDGRID_API_KEY", "")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
